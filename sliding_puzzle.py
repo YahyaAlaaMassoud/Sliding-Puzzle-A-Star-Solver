1541c474652a78f8c303d197e72ae7cc5cac65e5
Yahya Alaa <yehiaalaamassoud@gmail.com>
2018-03-14 00:43:48 +0200

    Added BFS solver + CLI

--- sliding_puzzle.py
+++ sliding_puzzle.py
@@ -2,7 +2,7 @@ import numpy as np
 from solver import Solver
 import sys, getopt
 
-def A_star(init_state, goal_state, max_iter, heuristic):
+#samples
 #goal_state = np.array([[1, 2, 3],
 #                       [4, 5, 6],
 #                       [7, 8, 0]])
@@ -10,8 +10,47 @@ def A_star(init_state, goal_state, max_iter, heuristic):
 #init_state = np.array([[1, 8, 7],
 #                       [3, 0, 5],
 #                       [4, 6, 2]])
+
+def BFS(init_state, goal_state, max_iter, heuristic):
     solver = Solver(init_state, goal_state, heuristic, max_iter)
-    path = solver.solve()
+    path = solver.solve_bfs()
+    
+    if len(path) == 0:
+        exit(1)
+    
+    init_idx = init_state.flatten().tolist().index(0)
+    init_i, init_j = init_idx // goal_state.shape[0], init_idx % goal_state.shape[0]
+    
+    print()
+    print('INITIAL STATE')
+    for i in range(goal_state.shape[0]):
+        print(init_state[i, :]) 
+    print()
+    for node in reversed(path):
+        cur_idx = node.get_state().index(0)
+        cur_i, cur_j = cur_idx // goal_state.shape[0], cur_idx % goal_state.shape[0]
+        
+        new_i, new_j = cur_i - init_i, cur_j - init_j
+        if new_j == 0 and new_i == -1:
+            print('Moved UP    from ' + str((init_i, init_j)) + ' --> ' + str((cur_i, cur_j)))
+        elif new_j == 0 and new_i == 1:
+            print('Moved DOWN  from ' + str((init_i, init_j)) + ' --> ' + str((cur_i, cur_j)))
+        elif new_i == 0 and new_j == 1:
+            print('Moved RIGHT from ' + str((init_i, init_j)) + ' --> ' + str((cur_i, cur_j)))
+        else:
+            print('Moved LEFT  from ' + str((init_i, init_j)) + ' --> ' + str((cur_i, cur_j)))
+        print('Score using ' + heuristic + ' heuristic is ' + str(node.get_score() - node.get_level()) + ' in level ' + str(node.get_level()))
+    
+        init_i, init_j = cur_i, cur_j
+        
+        for i in range(goal_state.shape[0]):
+            print(np.array(node.get_state()).reshape(3,3)[i, :]) 
+        print()
+    print(solver.get_summary())
+
+def A_star(init_state, goal_state, max_iter, heuristic):
+    solver = Solver(init_state, goal_state, heuristic, max_iter)
+    path = solver.solve_a_star()
     
     if len(path) == 0:
         exit(1)
@@ -49,9 +88,10 @@ def A_star(init_state, goal_state, max_iter, heuristic):
 def main(argv):
     max_iter = 5000
     heuristic = "manhattan"
+    algorithm = "a_star"
     
     try:
-        opts, args = getopt.getopt(argv,"h",["mx=","heur="])
+        opts, args = getopt.getopt(argv,"h",["mx=", "heur=", "astar", "bfs"])
     except getopt.GetoptError:
         print('python sliding_puzzle.py --mx <maximum_nodes> --heur <heuristic>')
         sys.exit(2)
@@ -64,6 +104,10 @@ def main(argv):
         elif opt in ("--heur"):
             if arg == "manhattan" or arg == "misplaced_tiles":
                 heuristic = (arg)
+        elif opt in ("--astar"):
+            algorithm = "a_star"
+        elif opt in ("--bfs"):
+            algorithm = "bfs"
     
     while True:
         try:
@@ -85,7 +129,10 @@ def main(argv):
     init_state = np.array(init_state).reshape(3, 3)
     goal_state = np.array(goal_state).reshape(3, 3)
     
-    A_star(init_state, goal_state, max_iter, heuristic)
+    if algorithm == "a_star":
+        A_star(init_state, goal_state, max_iter, heuristic)
+    else:
+        BFS(init_state, goal_state, max_iter, heuristic)
     
 if __name__ == "__main__":
     main(sys.argv[1:])
